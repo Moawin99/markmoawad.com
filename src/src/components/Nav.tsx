09d8a18a2cee955dcadde1a97e30b3aed2eb4013
Mark Moawad <msmoawad99@gmail.com>
2025-02-08 14:46:12 -0500

    wip: adding homeBlock component and styling

---
+++ src/src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import { motion } from 'framer-motion'
+export const Nav = () => {
+    return (
+        <nav className="flex mx-8 mb-24 pt-12 pb-6 justify-between align-middle flex-row">
+            <a href="/">Home</a>
+            <a href="/">Projects</a>
+            <a href="/">Resume</a>
+            <a href="/">Media</a>
+            <a href="/">About</a>
+        </nav>
+    );
+};
