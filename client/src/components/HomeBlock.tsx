d564caf83d38f1faa530da0a2b20d3422dcf7909
Mark Moawad <msmoawad99@gmail.com>
2025-03-01 15:10:55 -0500

    wip: made homepage reflexive

--- client/src/components/HomeBlock.tsx
+++ client/src/components/HomeBlock.tsx
@@ -8,9 +8,10 @@ export interface HomeBlockProps {
 }
 
 const HomeBlock = ({ title, description, fullWidth, subTitle }: HomeBlockProps) => {
+    
     return (
-        <div className={`flex flex-col justify-between p-6 bg-white rounded-xl shadow-sm ${fullWidth ? 'col-span-2' : 'col-span-1'}
-        md:col-span-${fullWidth ? '2': '1'} lg:col-span-${fullWidth ? '2': '1'} ${fullWidth ? 'row-span-2' : 'row-span-1'}`}>
+        <div className={`flex flex-col justify-between p-6 bg-white rounded-xl shadow-sm col-span-1 row-span-1
+        ${fullWidth ? 'md:col-span-2 md:row-span-2' : 'md:col-span-1 md:row-span-1'}`}>
             <div className="box-border flex flex-row justify-between w-full pr-3">
                 <h3>{subTitle}</h3>
                 <img src={pyramid} alt="pyramid" />
