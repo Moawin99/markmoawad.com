a9f4630346517cd5428522f26521c5b75af256fb
Mark Moawad <msmoawad99@gmail.com>
2025-04-02 19:19:42 -0400

    wip: fixing responsiveness on nav bar

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,6 +1,6 @@
 export default () => {
     return (
-        <nav className="flex pt-6 pl-12 pr-12 pb-6 sm:w-1/2 sm:pl-4 sm:pr-4 justify-between self-center flex-row">
+        <nav className="flex pl-4 pr-4 pt-6 md:pl-12 md:pr-12 pb-6 justify-between w-full md:w-1/2 sm:pl-4 sm:pr-4 sm:self-center flex-row">
             <a href="/">Home</a>
             <a href="/projects">Projects</a>
             <a href="/resume">Resume</a>
